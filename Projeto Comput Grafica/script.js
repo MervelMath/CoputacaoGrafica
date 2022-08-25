880142958c7d06c4626824a16774e73cce7c9878
Matheus <matheusdefigueiredo@gmail.com>
2022-08-25 11:46:01 -0300

    Aula do dia 25/08. Rotação e translação.

--- Projeto Comput Grafica/script.js
+++ Projeto Comput Grafica/script.js
@@ -3,15 +3,57 @@
 var canvas = document.getElementById("tela");
 var ctx = canvas.getContext("2d");
 
-var x = 100, y = 50, larg = 300, alt = 150;
+var x = 200, y = 200, larg = 10, alt = 10;
+var ang = 20;
+
+// function desenhar(){
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    
+    
+//     ctx.save(); //Salva o retângulo no começo (y=0).
+//         //Desenhando retângulo:
+//         ctx.translate(x,y); //Move o retângulo para um ponto que queremos. Senão ele fica cortado, pois desenhamos ele no y=0.
+//         ctx.fillStyle = "rgb(0, 0,0)";
+//         ctx.fillRect(-larg/2, 0, larg, alt); //Desenho do retângulo, usando as coordenadas do gráfico.
+//         //Restaura o retêngulo para o último save.
+        
+//         //Após restaurarmos, o retângulo é movido rapidamente para a posição especificada no translate,
+//         // e não percebemos que ele voltou ao início.
+
+//         //Desenhando um triângulo em cima do retângulo:
+//         ctx.fillStyle = "rgb(100, 100,0)";
+//         ctx.beginPath();
+//         ctx.moveTo(-larg/2,0);
+//         ctx.lineTo(0, -alt);
+//         ctx.lineTo(larg/2, 0);
+//         ctx.fill();
+
+//     ctx.restore();
+//     desenharTriangulo();
+//     requestAnimationFrame(desenhar);
+// }
 
 function desenhar(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "rgb(0, 0,0)";
-    ctx.fillRect(x, y, larg, alt);
-    desenharTriangulo();
+    ctx.strokeStyle = "rgb(50, 200, 200)";
+    ctx.lineWidth = 2;
+    ctx.save();
+        ctx.translate(x, y);
+        //ctx.rotate(ang);
+        var ca = Math.cos(ang);
+        var sa = Math.sin(ang);
+        ctx.transform(ca, sa, -sa, ca, 0, 0); //Criando rotação manualmente, com matriz.
+        ctx.beginPath();
+            ctx.moveTo(0,0);
+            ctx.lineTo(50,0);
+        ctx.stroke();
+        ctx.fillRect(-larg/2, -alt/2, larg, alt);
+    ctx.restore();
     requestAnimationFrame(desenhar);
 }
+    
 
 function desenharTriangulo(){
     ctx.beginPath();
@@ -31,12 +73,14 @@ function desenharTriangulo(){
     ctx.fill();
  }
 
-function leftArrowPressed() {
+    function leftArrowPressed() {
     x -= 5;
+    ang += 10;
     }
 
     function rightArrowPressed() {
     x += 5;
+    ang -= 10;
     }
 
     function upArrowPressed() {
